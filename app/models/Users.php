7eff2a2829185e0e41c199ee80e623e18c26c352
cosminmitroi <cosminj29@gmail.com>
2012-08-31 16:03:31 +0300

    Modules Users

---
+++ app/models/Users.php
@@ -0,0 +1,62 @@
+<?php
+namespace app\models;
+use lithium\security\Password;
+
+class Users extends  \lithium\data\Model{
+	
+		    //Basic validation
+public $validates = array(
+	        'username' => array(
+	            array('notEmpty', 'message'=>'You must include a username.'),
+		        ),
+	        'password' => array(
+	            array('notEmpty', 'message'=>'You must include a password.')
+	        ),
+	        'firstname' => array(
+			    array('notEmpty', 'message'=>'You must include your First Name.')
+			),
+			'lastname' => array(
+			    array('notEmpty','message'=>'You must include your Last Name.')
+			 ),
+			 'email' => array(
+			     array('notEmpty','message'=>'Email cannot be empty.'),
+			   
+			 ),
+			 
+	    );
+		
+protected static $_current = null;
+
+public static function current($refreshFromDatabase = false) {
+	      if (!$refreshFromDatabase && static::$_current) {
+	            return static::$_current;
+	        }
+	        if ($refreshFromDatabase) {
+	          
+	            return $userEntity;
+	        }
+        $sessionData = Session::read('username'); 
+	        if (!$sessionData) {
+	           
+	        }
+        $userEntity = static::create($sessionData, array('exists' => true));
+	        static::$_current = $userEntity;
+			
+	        return $userEntity;
+    }    		
+
+	/*public function save($entity, $data = null, array $options = array()) {
+		
+            if(!$entity->exists()){
+			        $entity->password = lithium\util\String::hash($entity->password);
+			    }
+			
+			    return parent::save($entity, $data,  $options);
+	
+	}*/
+
+}
+
+
+
+?>
