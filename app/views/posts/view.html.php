2368b6af1adb2c9b4e12c2bf5e1730843163871e
cosminmitroi <cosminj29@gmail.com>
2012-08-31 16:17:14 +0300

    View Posts

---
+++ app/views/posts/view.html.php
@@ -0,0 +1,9 @@
+<article>
+	<h1><?= $post['title'];?></h1>
+	<p>
+		<?=$this->html->link('Delete', array('controller' => 'posts', 'action' => 'delete', 'args' => array($post['id'])),array('onclick' => 'return confirm("Do you want to delete this post?")'));?>
+	</p>
+	<p><?php echo nl2br($post['body']); ?></p>
+</article>
+
+   
