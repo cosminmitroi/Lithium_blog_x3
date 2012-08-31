abc37fd3f21fadc60fd82834f5dc36ed81dc236d
cosminmitroi <cosminj29@gmail.com>
2012-08-31 18:17:36 +0300

    edit users

---
+++ app/views/users/edit.html.php
@@ -0,0 +1,13 @@
+<h2>New User</h2>
+<?php if(isset($success) && $success):?>
+<?php else: ?>
+<?=$this->form->create($user); ?>
+	<?=$this->form->field('username', array('label'=>'Username')); ?>
+	<?=$this->form->field('firstname', array('label' => 'FirstName')); ?>
+	<?=$this->form->field('lastname', array('lable' => 'LastName'));?>
+	<?=$this->form->field('email', array('lable' => 'Email')); ?>
+	<?=$this->form->submit('Add User'); ?>
+<?=$this->form->end(); ?>
+<?php endif; ?> 
+
+ 
