d39c0609de49dada83139332912a7aa138823ea6
cosminmitroi <cosminj29@gmail.com>
2012-09-03 18:26:30 +0300

    users view

--- app/views/users/index.html.php
+++ app/views/users/index.html.php
@@ -1,25 +1,27 @@
 <h2>Users</h2>
 
 <?php foreach($users as $user){ ?>
-    <table class="nonospace" border="2">
+ <table class="nonospace" border="2">
     <thead style="background-color: #ddd">
     <tr>
     <td>UserName</td>
     <td>FirstName</td>
     <td>LastName</td>
     <td>Email</td>
-    <td>Delete Users</td>
-    </tr>  
+    <td style="text-align:center "><?=$this->html->image('user_edit.png')?></td>
+    <td style="text-align:center"><?=$this->html->image('user_delete.png')?></td>
+   </tr>  
     <thead>
     <tbody>
     	<tr>
-    	<td width="140"><h5><?=strtoupper($user->username) ?><h5></td>
-    	<td width="140"><h6><?=$user->firstname; ?><h6></td>
-    	<td width="140"><h6><?=$user->lastname; ?><h6></td>
-    	<td width="140"><h6><?=$user->email; ?><h6></td>
-    	<td width="7"><h6><?=$this->html->link('Delete',array('controller' => 'users','action'=>'delete','args' => array($user->id)),array('onclick' => 'return confirm("Do you want to delete this post?")')); ?></h6></td>		
+    	<td width="135"><h5><?= $this->html->link($user->username,'users/view/'.$user->id); ?></h5></td>	
+    	<td width="135"><h6><?=$user->firstname; ?><h6></td>
+    	<td width="135"><h6><?=$user->lastname; ?><h6></td>
+    	<td width="135"><h6><?=$user->email; ?><h6></td>
+    	<td width="7" style="text-align:center"><h6><?=$this->html->link('Edit',array('controller' => 'users', 'action' => 'edit','args' => array($user->id)));?></h6>
+       	<td width="7" style="text-align:center"><h6><?=$this->html->link('Delete',array('controller' => 'users','action'=>'delete',
+    	'args' => array($user->id)),array('onclick' => 'return confirm("Do you want to delete this post?")')); ?></h6></td>		
     	</tr>
     </tbody>
-
-    </table>
+ </table>
 <?php } ?>
