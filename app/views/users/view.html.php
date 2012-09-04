a3e7d34cc0f497ad47372d15a86107e7dd68b1ed
cosminmitroi <cosminj29@gmail.com>
2012-09-04 10:31:22 +0300

    view users

--- app/views/users/view.html.php
+++ app/views/users/view.html.php
@@ -1,11 +1,13 @@
 <article>
-	
 	<div style="margin: 100px auto auto 20px;">
          <h2><span><a href="/users/">&lArr;</span></h2>
 		<h2>Username: <?=$users['username']; ?></h2>
 		      <h4>First Name: <?=$users['firstname'];?></h4>
 		      <h4>Last Name: <?=$users['lastname']; ?></h4>
 		      <h4>E-Mail: <?=$users['email']; ?></h4>
+		     <?php if(!empty($users['gender'])){?>
+		      <h4>Gender: <?=$users['gender']?></h4>
+		      <?php }?>
 		    
         </div>                                      
 </article>
